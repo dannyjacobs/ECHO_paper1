6104b409900aa2e87935a82621d6a2f6e540c26e
Danny Jacobs <daniel.c.jacobs@asu.edu>
2016-09-23 21:36:43 -0500

    show glyphs

--- scripts/plot_GB_slices.py
+++ scripts/plot_GB_slices.py
@@ -3,7 +3,7 @@ import numpy as n,os,sys
 import sys,optparse
 from pylab import *
 from scipy import optimize
-from ECHO.plot_utils import get_interp_val
+from ECHO.plot_utils import get_interp_val,add_cut_glyph
 from ECHO.read_utils import read_map
 style.use('./echo.mplstyle')
 o = optparse.OptionParser()
@@ -57,7 +57,7 @@ rxtx_slice_H = get_interp_val(rxtx_map,alt,az+np.pi/2)
 
 #altfig = figure(figsize=(8,6))
 fig,axarr = subplots(2,2,figsize=(10,6),sharey=True,sharex=True)
-
+rfig,raxarr = subplots(2,2,figsize=(10,6),sharey=True,sharex=True)
 
 # todo:
 # plot E and H in seperate panels
@@ -65,6 +65,12 @@ fig,axarr = subplots(2,2,figsize=(10,6),sharey=True,sharex=True)
 # subtract the tx model
 # plot the rx model
 
+"""
+filenames = ['../data/acc_GB_2015_Nant_EWtx_EWrx_8_beam.fits',
+'../data/acc_GB_2015_Sant_NStx_NSrx_8_beam.fits',
+'../data/acc_GB_2015_Sant_EWtx_EWrx_8_beam.fits',
+'../data/acc_GB_2015_Nant_NStx_NSrx_8_beam.fits']
+"""
 
 E_color = 'k'
 H_color = 'k'
@@ -75,7 +81,15 @@ for i,filename in enumerate(filenames):
     #apply rotation calibrations
     if opts.exp: rot += rots[filename]*np.pi/180
     #load the tx subtracted model
-    beam_filename = filename.replace('.fits','-bicolog_legs_360.fits')
+    files = []
+    if True: #use the fitted transmitter map
+        if filenames[i].find('Nant_NS')!=-1:
+            beam_filename = filenames[i].replace('.fits','-137MHz_tx_farfield_rot10deg_dist9cm_rot.fits')
+        else:
+            beam_filename = filenames[i].replace('.fits','-137MHz_tx_farfield_dist6.8cm_rot.fits')
+    else:
+        beam_filename = filenames[i].replace('.fits','-bicolog_legs_360_rot.fits')
+    #beam_filename = filename.replace('.fits','-bicolog_legs_360.fits')
     M = read_map(beam_filename)
     M -= np.ma.mean(M[:3])
     M_slice_E = get_interp_val(M,alt,az+rot)
@@ -87,13 +101,14 @@ for i,filename in enumerate(filenames):
     ME_slice_E = get_interp_val(ME,alt,az+rot)
     ME_slice_H = get_interp_val(ME,alt,az+np.pi/2+rot)
     if rot==0:
-        col=0
+        col=0 #EW
     else:
-        col=1
+        col=1 #NS
     axarr[0,col].errorbar(alt*180/np.pi,M_slice_E,yerr=ME_slice_E,
             label=labels[i],color=E_color,fmt=ls[i])
     axarr[1,col].errorbar(alt*180/np.pi,M_slice_H,yerr=ME_slice_H,
             color=H_color,fmt=ls[i])
+
 
 #plot the rx model
 axarr[0,0].plot(alt*180/np.pi,rx_slice_E,'-k',lw=2)
@@ -101,14 +116,23 @@ axarr[0,1].plot(alt*180/np.pi,rx_slice_E,'-k',lw=2)
 axarr[1,0].plot(alt*180/np.pi,rx_slice_H,'-k',lw=2)
 axarr[1,1].plot(alt*180/np.pi,rx_slice_H,'-k',lw=2)
 #legend(loc='best')
-axarr[0,0].set_title('NS')
-axarr[0,1].set_title('EW')
+axarr[0,1].set_title('NS')
+axarr[0,0].set_title('EW')
 axarr[0,0].set_ylabel('E plane\n [dB V/m]')
 axarr[1,0].set_ylabel('H plane\n [dB V/m]')
 axarr[1,0].set_xlabel('$\\theta$ (deg)')
 axarr[1,1].set_xlabel('$\\theta$ (deg)')
+
+subplots_adjust(wspace=0,hspace=0)
+#add the E/H polarization slice glyph
+add_cut_glyph(parent_axes=axarr[0,1],parent_fig=fig,cut='NS',pol='NS')  #E plane NS pol
+add_cut_glyph(parent_axes=axarr[1,1],parent_fig=fig,cut='EW',pol='NS')  #H plane NS pol
+add_cut_glyph(parent_axes=axarr[0,0],parent_fig=fig,cut='EW',pol='EW')  #E plane EW pol
+add_cut_glyph(parent_axes=axarr[1,0],parent_fig=fig,cut='NS',pol='EW')  #H plane EW pol
+
+
 #subplots_adjust(hspace=0,wspace=0)
-ylim(-35,2)
+axarr[0,0].set_ylim(-35,2)
 for ax in axarr.ravel():
     ax.grid(which='both') #turn on everyones grid
     ax.xaxis.set_ticks(np.arange(-80,100,20))
